dec9a3cfbb2ae89472edd166d0092560d568a5c2
M4K4TT4CK <121465111+M4K4TT4CK@users.noreply.github.com>
2023-08-31 19:36:33 -0400

    Adjust entire library

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name='isItLikely',
+    packages=find_packages(include=['isItLikely']),
+    version='0.1.0',
+    description='Probablity Library',
+    author='M4K4TT4CK',
+    license='MIT',
+)
